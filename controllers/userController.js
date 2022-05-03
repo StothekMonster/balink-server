79fba93903108a84a63ccdd2e64b6afcc81743e7
Yaakov Hassoun <Yaakov.hassoun.613@gmail.com>
2022-05-03 22:54:25 +0300

    extra api functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const users = JSON.parse(fs.readFileSync(`${__dirname}/../data/users.json`));
+
+let users = JSON.parse(fs.readFileSync(`${__dirname}/../data/users.json`));
 
 exports.getAllUsers = (req, res) => {
   res.status(200).json({
@@ -11,26 +12,52 @@ exports.getAllUsers = (req, res) => {
 };
 
 exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined',
-  });
+  const id = +req.params.id;
+  const user = users.find((userObj) => userObj.id === id);
+
+  res.status(200).json({ status: 'success', data: { user } });
 };
+
 exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined',
+  const id = +req.params.id;
+  const firstName = req.params.first_name;
+  const lastName = req.params.last_name;
+  const age = +req.params.age;
+  const phone = +req.params.phone;
+
+  const user = users.find((userObj) => userObj.id === id);
+  user.first_name = firstName;
+  user.last_name = lastName;
+  user.age = age;
+  user.phone = phone;
+  fs.writeFile(`${__dirname}/../data/users.json`, JSON.stringify(users), () => {
+    res.status(201).json({
+      status: 'success',
+    });
   });
 };
+
 exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined',
+  const id = +req.params.id;
+  users = users.filter((userObj) => userObj.id !== id);
+  fs.writeFile(`${__dirname}/../data/users.json`, JSON.stringify(users), () => {
+    res.status(201).json({
+      status: 'success',
+    });
   });
 };
+
 exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined',
+  const newId = users[users.length - 1].id + 1;
+  // eslint-disable-next-line node/no-unsupported-features/es-syntax
+  const newUser = { id: newId, ...req.body };
+  users.push(newUser);
+  fs.writeFile(`${__dirname}/../data/users.json`, JSON.stringify(users), () => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user: newUser,
+      },
+    });
   });
 };
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(
     `App running on port ${port}, running on ${process.env.NODE_ENV}`
